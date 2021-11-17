2f7c56f5870c17b316c6b7a771ec8e239ffe436a
unknown <Ractive89@126.com>
2021-11-17 17:27:27 +0800

    create

---
+++ ractive-portfolio/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-9xl text-center text-blue-600 sm:bg-black sm:text-white">Hello React</h1>
+    </div>
+  );
+}
+
+export default App;
